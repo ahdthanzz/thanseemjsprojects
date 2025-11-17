aff6c7e3cc312ecd09ba54f555b6d7c973c79835
thanseem <thanseemahamed123@gmail.com>
2025-11-17 23:52:05 +0530

    this my practice

--- functions.js
+++ functions.js
@@ -1,77 +1,41 @@
-// Functions For JavaScript
-/* let a = 12, b = 13;
-function operation(a,b) {
-    console.log(a+b*a);
+/* let a = 12;
+let b = 24;
+            //this is parameters//
+function add(a, b=12) {
+    console.log(a+b);
+    console.log(a*b-a);
+
 }
-
-operation(a,b);
-operation(1,4) */;
-//default values yum set pannalam 
-
-
-//Scop Variable
-/* let a = 14, b = 15;
-let c;//Golabal variable
-function ope() {
-    c = (a * b);//Loacal Variable
-    return c;
-}
-
-console.log('output', typeof ope(2, 3));
+//call the function
+add(a,b);
  */
-/*let num1 = 122;
 
- function oddEvan(num1) {
-    if (num1 % 2 == 0) {
-        return "Even";
+//Scope variable
+/* let a = 12;
+let b = 24;
+
+//let c; //global variable
+
+function add(a, b) {
+    c = (a + b); 
+    return c ; 
+    //local variable
+
+}
+//call the function
+add(a, b);
+console.log("this is a local variable ::::: ",c);
+ */
+
+let no1;
+function oddEvan(no1) {
+    if (no1%2==0) {
+        return "Evan";
+
     }
-    else {
-        return "Odd";
+    else{
+        return "Odd"
     }
-
+    
 }
-console.log(oddEvan(num1)); */
-
-// //Arrow Function
-// let oddEvan = (a) => a % 2 == 0 ? "Even" : "Odd";
-// console.log(oddEvan(122));
-
-
-/* function operation(op, a, b) {
-    return op(a, b);
-}
-let add = (a, b) => a + b;
-function sub(a, b) {
-    return a - b;
-}
-console.log(operation(sub,2,6)); */
-
-// let x5 = (a) => a*5;
-// console.log(x5(4));
-// let a = 12, b = 45;
-// function add(a, b) {
-//     console.log(a - b);
-// }
-// add(a, b);
-
-// let num1 = 19;
-// function oddEvan(num1) {
-//     if (num1 % 2 == 0) {
-//         return 'Evan';
-//     }
-//     console.log("hello tahnseem");
-//     return 'Odd';
-// }
-
-// let oddEvan = oddOrEvan;
-// //console.log(oddEvan(num1));
-// console.log(oddOrEvan(num1));
-
-// callback function
-
-// let oddEvan =(num1) => num1 %2==0? 'Evan' : 'Odd';
-// console.log(oddEvan(4));
-
-
-let xa = (a) => a*5;
-console.log(xa(4));
+console.log(oddEvan(10))
