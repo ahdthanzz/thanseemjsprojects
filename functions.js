c547d7d75b2478960cbebccbe8456a11ee2b26ad
thanseem <thanseemahamed123@gmail.com>
2025-11-18 22:50:41 +0530

    this callback edit

--- functions.js
+++ functions.js
@@ -27,7 +27,7 @@ add(a, b);
 console.log("this is a local variable ::::: ",c);
  */
 
-let no1;
+/* let no1;
 function oddEvan(no1) {
     if (no1%2==0) {
         return "Evan";
@@ -38,4 +38,30 @@ function oddEvan(no1) {
     }
     
 }
-console.log(oddEvan(10))
+// console.log(oddEvan(10));
+let oddOrEvan = oddEvan;
+console.log(oddOrEvan(4)); */
+
+/* let a = 12;
+let oddOrEvan =(a)=> a%2==0 ? "Evan" : "Odd" ;
+console.log(oddOrEvan(10)); */
+/* 
+function operation(op, a, b) {
+    return op(a,b);
+
+}
+
+// this is function
+let add = (a, b) => a + b;
+let sub = (a, b) => a - b;
+
+console.log(operation(add,3,2)); */
+
+function cal(op, a, b,) {
+    return op(a, b);
+
+}
+
+let add = (a, b) => a = b;
+let sub = (a, b) => a - b;
+console.log(cal(sub, 4, 5));
